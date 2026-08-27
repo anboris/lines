6d537b2f7461ee4188c79be68df114dc5591f6e0
Andrey Gonyaev <andrey@gonyaev.com>
2026-08-28 00:53:41 +0300

    Add vite-plugin-html-inject for HTML partial support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
+import injectHTML from "vite-plugin-html-inject";
 
 export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5174,
   },
+  plugins: [injectHTML()],
 });
