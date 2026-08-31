94524b932056b076bb63537ca6f5aca9040f18bb
Andrey Gonyaev <andrey@gonyaev.com>
2026-08-31 14:40:39 +0300

    Add svg spriter

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import injectHTML from "vite-plugin-html-inject";
 import { ViteEjsPlugin } from "vite-plugin-ejs"; // [1] Add this import
 import textData from "./data.json"; // Load your central JSON file
+import svgSpriter from "vite-plugin-svg-spriter";
+import path from "path";
 
 export default defineConfig({
   base: "/lines/",
@@ -13,5 +15,11 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 5174,
   },
-  plugins: [injectHTML(), ViteEjsPlugin(textData)],
+  plugins: [
+    injectHTML(),
+    ViteEjsPlugin(textData),
+    svgSpriter({
+      svgFolder: path.resolve(import.meta.dirname, "src/assets/svg"),
+    }),
+  ],
 });
