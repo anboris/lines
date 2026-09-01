e3e89810cd8e16bffeb6bf357e919416acef453c
Andrey Gonyaev <andrey@gonyaev.com>
2026-09-01 12:37:22 +0300

    Add autoprefixer and browserslist for CSS vendor prefixing

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
+import autoprefixer from "autoprefixer";
 import postcssImport from "postcss-import";
 
 export default {
-  plugins: [
-    postcssImport(),
-    // Add any other plugins you have here, like tailwindcss or autoprefixer
-  ],
+  plugins: [postcssImport(), autoprefixer()],
 };
