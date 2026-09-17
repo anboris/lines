bb4fc7b702030f8da853e0d9e153774f4d0de65c
Andrey Gonyaev <andrey@gonyaev.com>
2026-09-17 12:20:06 +0300

    Reformat code

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import './style.css';
-import {initCarousel} from "./modules/carousel.js";
-import {initEmbeddedForm} from "./modules/embeddedForm.js";
-import {initMarquee} from "./modules/marquee.js";
-import {initNavigation} from "./modules/navigation.js";
-import {initPricing} from "./modules/price.js";
-import {initQuiz} from "./modules/quiz.js";
+import "./style.css";
+import { initCarousel } from "./modules/carousel.js";
+import { initEmbeddedForm } from "./modules/embeddedForm.js";
+import { initMarquee } from "./modules/marquee.js";
+import { initNavigation } from "./modules/navigation.js";
+import { initPricing } from "./modules/price.js";
+import { initQuiz } from "./modules/quiz.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initNavigation();
